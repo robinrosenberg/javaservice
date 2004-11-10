04074c06c641bf4c8b31ed438816d26050a8eae7
rutthenut <rutthenut@a359582e-5d53-4b04-ba41-64ba0b62abdc>
2004-11-10 12:07:37 +0000

    Updated header comments with LGPL info
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/javaservice/trunk@30 a359582e-5d53-4b04-ba41-64ba0b62abdc

--- SampleService/src/org/objectweb/javaservice/test/SampleService.java
+++ SampleService/src/org/objectweb/javaservice/test/SampleService.java
@@ -1,10 +1,32 @@
 /*
- * (c) Copyright 2004 Multiplan Consultants Ltd.
- * 
+ * JavaService - Windows NT Service Daemon for Java applications
+ *
+ * Copyright (C) 2004 Multiplan Consultants Ltd.
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
  * Project : SampleService
  * File    : SampleService.java
  * Created : 09-Nov-2004
  * Author  : John Rutter
+ * Abstract: Sample Java Application to show use of JavaService.
+ *
+ * Information about the JavaService software is available at the ObjectWeb
+ * web site. Refer to http://javaservice.objectweb.org for more details.
  *
  */
 package org.objectweb.javaservice.test;
