f6dadf1ffa0a1c5bdf527390379a0c96d7b926f9
rutthenut <rutthenut@a359582e-5d53-4b04-ba41-64ba0b62abdc>
2004-11-10 11:56:11 +0000

    Command files moved from source to binary directory
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/javaservice/trunk@28 a359582e-5d53-4b04-ba41-64ba0b62abdc

--- SampleService/src/testInstallSampleService.bat
+++
@@ -1,9 +0,0 @@
-call installSampleService c:\j2sdk1.4.2\jre\bin\server c:\javaservice\dev\SampleService\bin c:\javaservice\dev\JavaService\bin -manual
-
-net start SampleService
-@pause
-
-net stop SampleService
-@pause
-
-call uninstallSampleService c:\javaservice\dev\SampleService\bin
--- SampleService/src/uninstallSampleService.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if "%1" == "" goto nodir
-cd %1
-:nodir
-@echo on
-SampleService.exe -uninstall SampleService
-del SampleService.exe
-@pause
