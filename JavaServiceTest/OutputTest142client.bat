10e72de199f87496d1feaf3096ed97e3ad446342
rutthenut <rutthenut@a359582e-5d53-4b04-ba41-64ba0b62abdc>
2005-07-20 19:36:56 +0000

    J2SDK 1.4.2_07 installed on d: drive
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/javaservice/trunk@138 a359582e-5d53-4b04-ba41-64ba0b62abdc

--- JavaServiceTest/OutputTest142client.bat
+++ JavaServiceTest/OutputTest142client.bat
@@ -1,3 +1,3 @@
 @echo off
 
-call installOutputTest C:\j2sdk1.4.2 client
+call installOutputTest d:\j2sdk1.4.2_07 client
--- JavaServiceTest/OutputTest142server.bat
+++ JavaServiceTest/OutputTest142server.bat
@@ -1,3 +1,3 @@
 @echo off
 
-call installOutputTest C:\j2sdk1.4.2 server
+call installOutputTest d:\j2sdk1.4.2_07 server
