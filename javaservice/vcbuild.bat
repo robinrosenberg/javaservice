75d6731840bfea2e507085d9a231fb71f3536b37
rutthenut <rutthenut@a359582e-5d53-4b04-ba41-64ba0b62abdc>
2005-08-01 22:00:57 +0000

    Specify full path to vcvars32 batch file
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/javaservice/trunk@181 a359582e-5d53-4b04-ba41-64ba0b62abdc

--- javaservice/vcbuild.bat
+++ javaservice/vcbuild.bat
@@ -1,6 +1,7 @@
 @rem vcbuild batch file to perform Visual Studio build steps
 @rem first, need to set up command-line values to run visual studio commands
 @rem call vcvars32
+call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32"
 @rem use the exported project makefile for both JavaService targets
 pushd src
 nmake /nologo /s /f javaservice.mak CFG="JavaService - Win32 Release"
