dc21953dfd48da8dd2e45b89c3c555fdd4e2f2a6
rutthenut <rutthenut@a359582e-5d53-4b04-ba41-64ba0b62abdc>
2006-01-04 09:03:22 +0000

    Changed default SDK location (for convenience only)
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/javaservice/trunk@262 a359582e-5d53-4b04-ba41-64ba0b62abdc

--- javaservice/scripts/TestSampleService.bat
+++ javaservice/scripts/TestSampleService.bat
@@ -7,7 +7,7 @@
 
 setlocal
 @rem note that if JVM not found, service 'does not report an error' when startup fails, although event logged
-if "%JAVA_HOME%" == "" set JAVA_HOME=d:\j2sdk1.4.2_07\jre
+if "%JAVA_HOME%" == "" set JAVA_HOME=d:\j2sdk1.4.2_10\jre
 set JVMDIR=%JAVA_HOME%\jre\bin\server
 set JSBINDIR=%CD%
 set JSEXE=%JSBINDIR%\JavaService.exe
