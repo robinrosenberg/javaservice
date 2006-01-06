b8d432e9c265293f0c1064067f8f1f47fb0bb771
rutthenut <rutthenut@a359582e-5d53-4b04-ba41-64ba0b62abdc>
2006-01-06 08:40:02 +0000

    Copyright 2006
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/javaservice/trunk@266 a359582e-5d53-4b04-ba41-64ba0b62abdc

--- javaservice/src/LicenceCommand.cpp
+++ javaservice/src/LicenceCommand.cpp
@@ -1,7 +1,7 @@
 /*
  * JavaService - Windows NT Service Daemon for Java applications
  *
- * Copyright (C) 2004 Multiplan Consultants Ltd.
+ * Copyright (C) 2006 Multiplan Consultants Ltd.
  *
  *
  * This library is free software; you can redistribute it and/or
@@ -36,7 +36,7 @@
 //
 
 static const char* const COPY_TEXT
-  = "\n (c) Copyright 2005 Multiplan Consultants Limited.\n";
+  = "\n (c) Copyright 2006 Multiplan Consultants Limited.\n";
 
 static const char* const LGPL_TEXT
   = "\n This library is free software; you can redistribute it and/or"
