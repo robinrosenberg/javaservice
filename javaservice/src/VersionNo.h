2bda35457920cbc7197ddfadb2445a134fbaaf69
rutthenut <rutthenut@a359582e-5d53-4b04-ba41-64ba0b62abdc>
2006-05-01 09:13:41 +0000

    V2.0.10 build
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/javaservice/trunk@291 a359582e-5d53-4b04-ba41-64ba0b62abdc

--- javaservice/src/VersionNo.h
+++ javaservice/src/VersionNo.h
@@ -7,10 +7,10 @@
 #ifndef __VERSION_NO_H__
 #define __VERSION_NO_H__
 
-#define FILEVER         2,1,0,0
-#define PRODUCTVER      2,1,0,0
-#define STRFILEVER     "2,1,0,0\0"
-#define STRPRODUCTVER  "2,1,0,0\0"
+#define FILEVER         2,0,10,0
+#define PRODUCTVER      2,1,10,0
+#define STRFILEVER     "2,0,10,0\0"
+#define STRPRODUCTVER  "2,0,10,0\0"
 
 #endif // __VERSION_NO_H__
 
